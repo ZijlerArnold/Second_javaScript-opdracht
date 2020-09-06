d96520d879f7f405c96786da66c4f6f0a12d382b
ZijlerArnold <51839866+ZijlerArnold@users.noreply.github.com>
2020-09-06 18:06:16 +0200

    Add files via upload

---
+++ W1D2-3/index.js
@@ -0,0 +1,16 @@
+console.log('');
+console.log("              Hallo Winc Academy studenten");
+console.log('              =============================');
+console.log('');
+
+let num1 = 1230941;
+let num2 = 1823794;
+let num3 = 637263;
+let num4 = 54;
+// let niet = 'ik word genegeerd :-(';
+// console.log(niet); ik word niet uitgevoerd. :-(
+
+console.log(`uitkomst 1: ${num1} * ${num2}`);
+console.log(`uitkomst 2: ${num3} / ${num4}`);
+console.log('uitcommenten, toetsencombinatie CTRL-C-K and outcommenten CTRL-U-K');
+console.log('');
